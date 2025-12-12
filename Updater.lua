b1531a8198a5e021e77126a819f39cb7775ed4d5
CytokininWasTaken <48562707+CytokininWasTaken@users.noreply.github.com>
2025-12-12 21:27:28 +0100

    Update Updater.lua

--- Updater.lua
+++ Updater.lua
@@ -2,5 +2,6 @@ local url = "https://raw.githubusercontent.com/CytokininWasTaken/ccPrograms/refs
 local function writeFile(programUrl)
     shell.run("delete", "startup/BingusOS.lua")
     shell.run("wget", programUrl, "startup/BingusOS.lua")
+    shell.run("reboot")
 end
 writeFile(url)
