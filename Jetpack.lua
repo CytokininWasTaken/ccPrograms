b7c788e911074be461c20c79442fc1d18222f1f1
CytokininWasTaken <48562707+CytokininWasTaken@users.noreply.github.com>
2025-12-15 00:20:07 +0100

    Update Jetpack.lua

--- Jetpack.lua
+++ Jetpack.lua
@@ -4,8 +4,6 @@ local sleepTime = settings.get("sleepTime") or 1
 local inventoryManager = peripheral.wrap("back")
 local chatBox = peripheral.wrap("right")
 
-print(inventoryManager)
-
 local function getPlayerTank()
     for k, v in pairs(inventoryManager.getArmor()) do
         if v.slot == 102 then return v end
